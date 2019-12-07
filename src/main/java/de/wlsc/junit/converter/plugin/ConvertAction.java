1d71ed5b98a1795109ef2b69c801ee3fe27b9754
Wladimir Schmidt <wlsc.dev@gmail.com>
2019-12-08 00:25:24 +0100

    Added handling of @Ignore and contexted @Test annotation

--- src/main/java/de/wlsc/junit/converter/plugin/ConvertAction.java
+++ src/main/java/de/wlsc/junit/converter/plugin/ConvertAction.java
@@ -6,9 +6,23 @@ import static java.util.Objects.isNull;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.LambdaExpr;
+import com.github.javaparser.ast.expr.LongLiteralExpr;
 import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
+import com.github.javaparser.ast.expr.SingleMemberAnnotationExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.Statement;
 import com.google.common.collect.ImmutableMap;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -33,6 +47,7 @@ public class ConvertAction extends AnAction {
       .put("org.junit.BeforeClass", "org.junit.jupiter.api.BeforeAll")
       .put("org.junit.After", "org.junit.jupiter.api.AfterEach")
       .put("org.junit.AfterClass", "org.junit.jupiter.api.AfterAll")
+      .put("org.junit.Ignore", "org.junit.jupiter.api.Disabled")
       // annotations
       .put("Before", "BeforeEach")
       .put("BeforeClass", "BeforeAll")
@@ -84,15 +99,87 @@ public class ConvertAction extends AnAction {
   private void replaceImports(final CompilationUnit compilationUnit) {
     for (ImportDeclaration importDeclaration : compilationUnit.findAll(ImportDeclaration.class)) {
       Optional.ofNullable(JUST_REPLACERS.get(importDeclaration.getNameAsString()))
-          .ifPresent(replaceWith -> importDeclaration.replace(new ImportDeclaration(replaceWith, false, false)));
+          .map(name -> new ImportDeclaration(name, false, false))
+          .ifPresent(importDeclaration::replace);
     }
   }
 
   private void replaceAnnotations(final CompilationUnit compilationUnit) {
     for (AnnotationExpr annotationExpr : compilationUnit.findAll(AnnotationExpr.class)) {
-      Optional.ofNullable(JUST_REPLACERS.get(annotationExpr.getNameAsString()))
-          .ifPresent(replaceWith -> annotationExpr.replace(new MarkerAnnotationExpr(new Name(replaceWith))));
+
+      String name = annotationExpr.getNameAsString();
+
+      Optional.ofNullable(JUST_REPLACERS.get(name))
+          .map(Name::new)
+          .map(MarkerAnnotationExpr::new)
+          .ifPresent(annotationExpr::replace);
+
+      if ("Ignore".equals(name)) {
+        replaceIgnore(annotationExpr);
+      }
+
+      if ("Test".equals(name)) {
+        replaceTest(compilationUnit, annotationExpr);
+      }
     }
+  }
+
+  private void replaceIgnore(final AnnotationExpr annotationExpr) {
+    if (annotationExpr instanceof MarkerAnnotationExpr) {
+      annotationExpr.replace(new MarkerAnnotationExpr(new Name("Disabled")));
+    }
+    if (annotationExpr instanceof SingleMemberAnnotationExpr) {
+      StringLiteralExpr newValue = new StringLiteralExpr(((SingleMemberAnnotationExpr) annotationExpr).getMemberValue().asStringLiteralExpr().getValue());
+      annotationExpr.replace(new SingleMemberAnnotationExpr(new Name("Disabled"), newValue));
+    }
+  }
+
+  private void replaceTest(final CompilationUnit compilationUnit, final AnnotationExpr annotationExpr) {
+
+    if (annotationExpr instanceof NormalAnnotationExpr) {
+      NormalAnnotationExpr normalAnnotationExpr = (NormalAnnotationExpr) annotationExpr;
+      for (MemberValuePair pair : normalAnnotationExpr.getPairs()) {
+        normalAnnotationExpr.getParentNode().ifPresent(node -> {
+          for (Node childNode : node.getChildNodes()) {
+            if (childNode instanceof BlockStmt) {
+              String identifier = pair.getName().asString();
+              if ("timeout".equals(identifier)) {
+                wrapWithAssertTimeout(compilationUnit, (BlockStmt) childNode, pair.getValue());
+              }
+              if ("expected".equals(identifier)) {
+                wrapWithExpected(compilationUnit, (BlockStmt) childNode, pair.getValue());
+              }
+            }
+          }
+        });
+      }
+      normalAnnotationExpr.replace(new MarkerAnnotationExpr(new Name("Test")));
+    }
+  }
+
+  private void wrapWithAssertTimeout(final CompilationUnit compilationUnit, final BlockStmt oldBlockStmt, final Expression annotationValue) {
+
+    BlockStmt previousBlockStmt = new BlockStmt(oldBlockStmt.getStatements());
+    MethodCallExpr assertTimeout = new MethodCallExpr("assertTimeout",
+        new MethodCallExpr("ofMillis", new LongLiteralExpr(annotationValue.asLongLiteralExpr().asLong())),
+        new LambdaExpr(new NodeList<>(), previousBlockStmt));
+    Statement timeoutStatement = new ExpressionStmt(assertTimeout);
+    oldBlockStmt.setStatements(new NodeList<>(timeoutStatement));
+
+    compilationUnit.addImport("java.time.Duration.ofMillis", true, false);
+    compilationUnit.addImport("org.junit.jupiter.api.Assertions.assertTimeout", true, false);
+  }
+
+  private void wrapWithExpected(final CompilationUnit compilationUnit, final BlockStmt oldBlockStmt, final Expression annotationValue) {
+
+    BlockStmt previousBlockStmt = new BlockStmt(oldBlockStmt.getStatements());
+    MethodCallExpr assertTimeout = new MethodCallExpr("assertThrows",
+        new ClassExpr(annotationValue.asClassExpr().getType()),
+        new LambdaExpr(new NodeList<>(), previousBlockStmt));
+    Statement timeoutStatement = new ExpressionStmt(assertTimeout);
+    oldBlockStmt.setStatements(new NodeList<>(timeoutStatement));
+
+    compilationUnit.addImport("org.junit.jupiter.api.Assertions.assertThrows", true, false);
   }
 
   private void refreshProject(final AnActionEvent e) {
