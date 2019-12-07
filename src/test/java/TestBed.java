621ef6d6662e43449929abd4f8f344ac5341fe28
Wladimir Schmidt <wlsc.dev@gmail.com>
2019-12-07 11:45:57 +0100

    Added initial testbed

---
+++ src/test/java/TestBed.java
@@ -0,0 +1,61 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TestBed {
+
+  @Test
+  public void name0() {
+
+  }
+
+  @Test()
+  public void name1() {
+
+  }
+
+  @Test(timeout = 20L)
+  public void name2() {
+
+  }
+
+  @Test(expected = NullPointerException.class,timeout = 20L)
+  public void name21() {
+
+  }
+
+  @Test
+  @Ignore
+  public void name01() {
+
+  }
+
+  @Test
+  @Ignore("because")
+  public void name011() {
+
+  }
+
+  @Before
+  public void before() {
+
+  }
+
+  @BeforeClass
+  public static void before1() {
+
+  }
+
+  @After
+  public void after() {
+
+  }
+
+  @AfterClass
+  public static void after1() {
+
+  }
+}
